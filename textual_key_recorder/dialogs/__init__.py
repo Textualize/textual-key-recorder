cb8f211a5068f6a8303387c619eb9ab8af7459f7
Dave Pearson <davep@davep.org>
2023-10-05 21:27:57 +0100

    Add a simple yes/no dialog to the application

--- textual_key_recorder/dialogs/__init__.py
+++ textual_key_recorder/dialogs/__init__.py
@@ -1,5 +1,6 @@
 """Defines dialogs for use in the application."""
 
 from .annotation import Annotation
+from .yes_no import YesNo
 
-__all__ = ["Annotation"]
+__all__ = ["Annotation", "YesNo"]
---
+++ textual_key_recorder/dialogs/yes_no.py
@@ -0,0 +1,62 @@
+"""Provides a simple yes/no dialog."""
+
+from textual import on
+from textual.app import ComposeResult
+from textual.containers import Horizontal, Vertical
+from textual.screen import ModalScreen
+from textual.widgets import Button, Label
+
+
+class YesNo(ModalScreen[bool]):
+    """A simple yes/no dialog."""
+
+    DEFAULT_CSS = """
+    YesNo {
+        align: center middle;
+    }
+
+    Vertical {
+        width: auto;
+        height: auto;
+        border: thick $accent;
+        padding: 1 2;
+        background: $panel;
+    }
+
+    Horizontal {
+        align: right middle;
+        width: auto;
+        height: auto;
+        margin-top: 1;
+    }
+
+    Button {
+        margin-left: 1;
+    }
+    """
+
+    def __init__(self, question: str = "") -> None:
+        """Initialise the question dialog.
+
+        Args:
+            question: The question to ask.
+        """
+        super().__init__()
+        self._question = question
+
+    def compose(self) -> ComposeResult:
+        """Compose the content of the dialog."""
+        with Vertical():
+            yield Label(self._question)
+            with Horizontal():
+                yield Button("Yes", id="yes")
+                yield Button("No", id="no")
+
+    @on(Button.Pressed)
+    def decision(self, event: Button.Pressed) -> None:
+        """Handle the user's decision.
+
+        Args:
+            event: The button press event that is the decision.
+        """
+        self.dismiss(event.button.id == "yes")
